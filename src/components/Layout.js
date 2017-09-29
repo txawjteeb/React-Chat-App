476474c7ccff312dfa0de4899c3e888184754a76
David Lee <itxawjteeb@gmail.com>
2017-09-29 02:53:12 -0700

    Building the first part of the Socket.io Chat Application in ReactJS

---
+++ src/components/Layout.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+import { USER_CONNECTED, LOGOUT } from '../Events'
+import LoginForm from './LoginForm';
+
+const socketUrl = "http://192.168.1.103:3231"
+export default class Layout extends Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			socket:null,
+			user:null
+		}
+	}
+
+	componentWillMount() {
+		this.initSocket()
+	}
+
+	/*
+	*	Connect to and initializes the socket.
+	*/
+	initSocket = ()=>{
+		const socket = io(socketUrl)
+
+		socket.on('connect', ()=>{
+			console.log("Connected");
+		})
+		this.setState({socket});
+	}
+
+	/*
+	*	Sets the user property in state
+	*	@param user {id:number, name:string}
+	*/
+	setUser = (user)=>{
+		const { socket } = this.state;
+		socket.emit(USER_CONNECTED, user);
+		this.setState({user})
+	}
+
+	/*
+	*	Sets the user property in state to null.
+	*/
+	logout = ()=>{
+		const { socket } = this.state;
+		socket.emit(LOGOUT);
+		this.setState({user:null});
+
+	}
+
+	render() {
+		const { title } = this.props;
+		const { socket } = this.state;
+		return (
+			<div className="container">
+				<LoginForm socket={socket} setUser={this.setUser} />
+			</div>
+		);
+	}
+}
